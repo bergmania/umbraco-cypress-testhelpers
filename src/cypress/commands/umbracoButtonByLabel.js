4ede900411d603ef19e0694d0a0090a12caec9c2
Bjarke Berg <mail@bergmania.dk>
2020-02-19 22:20:39 +0100

    More cypress commands

---
+++ src/cypress/commands/umbracoButtonByLabel.js
@@ -0,0 +1,18 @@
+﻿import CommandBase from "./commandBase";
+
+export default class UmbracoButtonByLabel extends CommandBase {
+  _commandName = 'umbracoButtonByLabel';
+
+  method(label) {
+    const cy = this.cy;
+    const cypress = this.cypress;
+
+    cypress.log({
+      displayName: "Umbraco Button",
+    });
+
+    return cy.get('umb-button[label="'+label+'"] button:enabled', {log:false});
+  }
+}
+
+
