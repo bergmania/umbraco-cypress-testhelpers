806eebfcac647fb70a834dc92c02379d819fa635
Thomas Andresen <tha@umbraco.dk>
2020-05-13 13:25:47 +0200

    Cherry-picking post install fix

--- postinstall.js
+++ postinstall.js
@@ -2,16 +2,17 @@ const ncp=require('ncp').ncp;
 const fs = require('fs');
 const path= require('path');
 
-const basepath = process.argv.slice(2)[0];
-const src = path.join(basepath,'node_modules','umbraco-cypress-testhelpers','src','cypress','commands','chainable.ts');
-const dst = path.join(basepath,'cypress','support', 'chainable.ts');
+const basepath = __dirname;
+const src = path.join(basepath,'src','cypress','commands','chainable.ts');
+const dst = path.join(basepath,'..','..','cypress','support', 'chainable.ts');
 
 const fixturesSrc=path.join(basepath,'node_modules','umbraco-cypress-testhelpers','cypress','fixtures','prevalueSourceFile.txt');
 const fixturesDst = path.join(basepath,'cypress','fixtures', 'prevalueSourceFile.txt');
 
-try {
+try {  
   if (fs.existsSync(src)) {          
     console.log('Copy chainable.ts file');
+    
     ncp(src, dst);    
   }
 } catch(err) {
