d6e7884638a8276670938163c2c0901797d7bc95
Thomas Andresen <tha@umbraco.dk>
2020-05-25 13:58:09 +0200

    Fixed timing issue

--- cypress/integration/Forms/dataSourceTest.ts
+++ cypress/integration/Forms/dataSourceTest.ts
@@ -12,7 +12,7 @@ context('Forms Data sources', () => {
     });
 
     afterEach(() => {
-        dataSources.cleanUp();        
+        //dataSources.cleanUp();        
     });
 
 
@@ -32,12 +32,17 @@ context('Forms Data sources', () => {
     it('Test CreateSql Database source', () => {
         const dataSourceName = faker.random.word();
         var fixture=  {Connection: 'Server=myServerAddress;Database=myDataBase;User Id=myUsername;Password=myPassword;Provider=sqloledb',Table: 'Test'}
-        dataSources.insertSqlDatabase(dataSourceName,fixture).then(dataSource => {            
-            cy.visit(`/umbraco#/forms/datasource/edit/${dataSource.id}`).wait(15000).then(()=>{
-            cy.dataUmbScope(`settingstype-pickers-dataSourceType`).its('dataSource.formDataSourceTypeId').should('deep.equal',dataSource.formDataSourceTypeId);
-            cy.dataUmbScope(`setting-dataSource_0`,`textarea`).its('setting.value').should('deep.equal',fixture.Connection);
-            cy.dataUmbScope(`setting-dataSource_1`,`input`).its('setting.value').should('deep.equal',fixture.Table);            
-        });
+        
+        dataSources.insertSqlDatabase(dataSourceName,fixture).then(dataSource => {  
+            cy.server();
+            cy.route(`/umbraco/backoffice/UmbracoForms/DataSource/GetByGuid?guid=${dataSource.id}`).as('wait');  // start recording requests                   
+                                
+            cy.visit(`/umbraco#/forms/datasource/edit/${dataSource.id}`).then(()=>{
+                cy.wait('@wait').its('status').should('be', 200);                    
+                cy.dataUmbScope(`settingstype-pickers-dataSourceType`).its('dataSource.formDataSourceTypeId').should('deep.equal',dataSource.formDataSourceTypeId);
+                cy.dataUmbScope(`setting-dataSource_0`,`textarea`).its('setting.value').should('deep.equal',fixture.Connection);
+                cy.dataUmbScope(`setting-dataSource_1`,`input`).its('setting.value').should('deep.equal',fixture.Table);            
+            });
         });
     });    
 });
