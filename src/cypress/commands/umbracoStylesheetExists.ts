8a1f739eaf7d3ad33c32b2a0d42fd7e7051a017c
Mole <nikolajlauridsen@protonmail.ch>
2020-09-04 13:58:37 +0200

    Fix endpoint

--- src/cypress/commands/umbracoStylesheetExists.ts
+++ src/cypress/commands/umbracoStylesheetExists.ts
@@ -2,5 +2,5 @@ import UmbracoFileExists from './umbracoFileExists';
 
 export default class UmbracoStyleSheetExists extends UmbracoFileExists{
     _commandName = 'umbracoStylesheetExists';
-    _endPoint = 'BackOffice/Api/StylesheetsTree/GetNodes?id=-1';
+    _endPoint = '/BackOffice/Api/StylesheetsTree/GetNodes?id=-1';
 }
