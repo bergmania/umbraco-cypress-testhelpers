d7f8dacd2cd1e8e21ddb5c9c25d142795baf8062
Mole <nikolajlauridsen@protonmail.ch>
2020-09-25 11:37:37 +0200

    Enable specifying allowed editors and max items in row config

--- src/cms/builders/dataTypes/gridBuilders/gridAreaBuilder.ts
+++ src/cms/builders/dataTypes/gridBuilders/gridAreaBuilder.ts
@@ -4,10 +4,13 @@ export class GridAreaBuilder {
   parentBuilder;
   gridSize;
   editors;
+  allowed;
+  maxItems;
 
   constructor(parentBuilder: GridLayoutBuilder) {
     this.parentBuilder = parentBuilder;
     this.editors = [];
+    this.allowed = [];
   }
 
   withGridSize(gridSize: number) {
@@ -15,8 +18,48 @@ export class GridAreaBuilder {
     return this;
   }
 
-  withEditors(editor) {
+  withMaxItems(maxItems : number) {
+    this.maxItems = maxItems;
+    return this
+  }
+
+  withEditor(editor) {
     this.editors.push(editor);
+    return this;
+  }
+
+  withAllowRTE() {
+    this.allowed.push('rte');
+    return this;
+  }
+
+  withAllowImage() {
+    this.allowed.push('media');
+    return this;
+  }
+
+  withAllowMacro() {
+    this.allowed.push('macro');
+    return this;
+  }
+
+  withAllowEmbed() {
+    this.allowed.push('embed');
+    return this;
+  }
+
+  withAllowHeadline() {
+    this.allowed.push('headline');
+    return this;
+  }
+
+  withAllowQuote() {
+    this.allowed.push('quote');
+    return this;
+  }
+
+  withAllowAll() {
+    this.allowed = [];
     return this;
   }
 
@@ -25,13 +68,23 @@ export class GridAreaBuilder {
   }
 
   build() {
-    if (this.editors.length > 0) {
-      return {
-        grid: this.gridSize,
-        editors: this.editors,
-      };
-    } else {
-      return { grid: this.gridSize };
+    let area = { grid: this.gridSize }
+
+    if(this.editors.length > 0) {
+      Object.assign(area, {editors: this.editors});
     }
+
+    if(this.allowed.length > 0) {
+      Object.assign(area, {
+        allowAll: false,
+        allowed: this.allowed,
+      })
+    }
+
+    if(this.maxItems) {
+      Object.assign(area, { maxItems : this.maxItems})
+    }
+
+    return area;
   }
 }
