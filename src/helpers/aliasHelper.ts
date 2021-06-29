a14e6c78a567f23a9795bf6bdf7ce1b6388b91d8
Warren Buckley <warren@umbraco.com>
2021-06-29 08:46:12 +0100

    Adds JSDcos to AliasHelper

--- src/helpers/aliasHelper.ts
+++ src/helpers/aliasHelper.ts
@@ -1,20 +1,51 @@
 import camelize from 'camelize';
 
 export class AliasHelper {
-  static toSafeAlias(text) {
+
+  /**
+   * Uses Camelize npm library to generate a safe alias from a string
+   * that may contain spaces and dashes etc
+   * 
+   * @param  {string} text Input string
+   * @returns {string} A camelcased string that starts with 'a' and ends with 'a'
+   * @see {@link https://www.npmjs.com/package/camelize}
+   */
+   static toSafeAlias(text: string): string {
     return 'a' + camelize(text) + 'a';
   }
 
-  static toAlias(text) {
+  
+  /**
+   * Camel cases a string by calling the toCamelCase() method
+   * 
+   * @param  {string} text Input string
+   * @returns {string} A camelcased string
+   */
+  static toAlias(text:string):string {
     return this.toCamelCase(text);
   }
 
-  static capitalize(text) {
+
+  /**
+   * Capatilze a string
+   * 
+   * @param  {string} text Input string
+   * @returns {string} A capatilized string, of the first character only
+   */
+  static capitalize(text:string):string {
     if (typeof text !== 'string') return '';
     return text.charAt(0).toUpperCase() + text.slice(1);
   }
 
-  static toCamelCase(sentenceCase) {
+
+  /**
+   * Convert a sentence into camelCase
+   * `toCamelCase('My aWesome Example')` would return `myAwesomeExample`
+   * 
+   * @param  {string} sentenceCase Input string
+   * @returns {string} A camel cased string
+   */
+  static toCamelCase(sentenceCase:string):string {
     let out = '';
     sentenceCase.split(' ').forEach((el, idx) => {
       const add = el.toLowerCase();
@@ -23,7 +54,14 @@ export class AliasHelper {
     return out;
   }
 
-  static uuidToAlias(uuid: string) {
+  
+  /**
+   * Removes dashes from UUID string
+   * 
+   * @param  {string} uuid A string representing a UUID
+   * @returns {string} UUID without dashes
+   */
+  static uuidToAlias(uuid: string):string {
     uuid = uuid.replace(/-/g, '');
     return this.toAlias(uuid);
   }
