0d458e57c181815fd3aaf6c1ffe0ac25c27a0b6e
Warren Buckley <warren@umbraco.com>
2021-06-29 09:43:02 +0100

    Adds jsdoc for the MinimalTemplate.get()

--- src/cms/templates/minimalTemplate.ts
+++ src/cms/templates/minimalTemplate.ts
@@ -1,7 +1,15 @@
 import { AliasHelper } from '../../helpers/aliasHelper';
 
 export class MinimalTemplate {
-  public get(model = '', properties?: { name: string; alias: string }[]): string {
+  
+  /**
+   * Generates a HTML string for a basic HTML Razor view template
+   * 
+   * @param  {string} model Name of model to use in HTML Razor View template. Default value is empty string
+   * @param  {{name:string;alias:string}[]} properties Optional array of objects containing `name` and `alias` which will use to print out the Document type property values in the view
+   * @returns string A basic HTML Razor view with the correct model for the doctype and prints out values for each property
+   */
+  public get(model:string = '', properties?: { name: string; alias: string }[]): string {
     if (model.length > 0) model = `<${AliasHelper.capitalize(model)}>`;
     let template = `@inherits Umbraco.Web.Mvc.UmbracoViewPage${model}
         @{
