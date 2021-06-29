58939b07f0e28a2d09c81ebbbd449e08980a678b
Warren Buckley <warren@umbraco.com>
2021-06-29 09:30:28 +0100

    Marks responseHelper as deprecated

--- src/helpers/responseHelper.ts
+++ src/helpers/responseHelper.ts
@@ -5,10 +5,11 @@ export class ResponseHelper {
    * as it assumes it will **always** contain the junk json data `)]}',\n`
    * 
    * It would be better to use jsonHelper.getBody() 
-   * as it checks for its presence of the data   * 
+   * as it checks for its presence of the data 
    * 
    * @param response The raw HTTP response from the server
    * @returns {object} The JSON data in the body of the response as an object 
+   * @deprecated Please use jsonHelper.getBody instead
    * @see jsonHelper.getBody()
    */
   static getResponseBody(response):object {
