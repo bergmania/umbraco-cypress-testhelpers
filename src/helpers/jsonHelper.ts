ce05fcc54c837e8f1eaabf05fa753247ff827187
Warren Buckley <warren@umbraco.com>
2021-06-29 09:29:48 +0100

    Typescript eslint rule suggests not to use 'object' as type

--- src/helpers/jsonHelper.ts
+++ src/helpers/jsonHelper.ts
@@ -1,15 +1,13 @@
 export class JsonHelper {
-
-  
   /**
    * Expects the HTTP body response to be JSON
    * This will remove the `)]}',\n` if present
    * and return the correct JSON data as an object
-   * 
+   *
    * @param response The raw HTTP response from the server
-   * @returns {object} The JSON data in the body of the response as an object
+   * @returns The JSON data in the body of the response as an object
    */
-  static getBody(response):object {
+  static getBody(response) {
     const junk = ")]}',\n";
     let json = response.body;
 
