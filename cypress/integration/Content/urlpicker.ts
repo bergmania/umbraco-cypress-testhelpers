a37780acb6d977e5de9d0fdd0e52aa4c001ea31e
Zeegaan <70372949+Zeegaan@users.noreply.github.com>
2021-09-03 12:55:04 +0200

    Fixed url picker test so it waits properly

--- cypress/integration/Content/urlpicker.ts
+++ cypress/integration/Content/urlpicker.ts
@@ -12,13 +12,11 @@ context('Url Picker', () => {
       });
 
     it('Test Url Picker', () => {
-
-        cy.deleteAllContent();
+       
         const urlPickerDocTypeName = 'Url Picker Test';
         const pickerDocTypeAlias = AliasHelper.toAlias(urlPickerDocTypeName); 
-
         cy.umbracoEnsureDocumentTypeNameNotExists(urlPickerDocTypeName);
-        
+        cy.deleteAllContent();
         const pickerDocType = new DocumentTypeBuilder()
             .withName(urlPickerDocTypeName)
             .withAlias(pickerDocTypeAlias)
@@ -33,9 +31,17 @@ context('Url Picker', () => {
             .build();
 
         cy.saveDocumentType(pickerDocType);
-
+        cy.editTemplate(urlPickerDocTypeName, '@inherits Umbraco.Web.Mvc.UmbracoViewPage<ContentModels.UrlPickerTest>' +
+        '\n@using ContentModels = Umbraco.Web.PublishedModels;' +
+        '\n@{' +
+        '\n    Layout = null;' +
+        '\n}' +
+        '\n@foreach(var link in @Model.Picker)' +
+        '\n{' +
+        '\n    <a href="@link.Url">@link.Name</a>' +
+        '\n}');
         // Create content with url picker
-        cy.get('.umb-tree-root-link').rightclick();
+        cy.get('li .umb-tree-root:contains("Content")').should("be.visible").rightclick();
         cy.get('[data-element="action-create"]').click();
         cy.get('[data-element="action-create-' + pickerDocTypeAlias + '"] > .umb-action-link').click();
         // Fill out content
@@ -46,28 +52,15 @@ context('Url Picker', () => {
         // Should really try and find a better way to do this, but umbracoTreeItem tries to click the content pane in the background
         cy.get('#treePicker li:first', {timeout: 6000}).click();
         cy.get('.umb-editor-footer-content__right-side > [button-style="success"] > .umb-button > .btn > .umb-button__content').click();
-        // We have to wait for the picked content to show up or it wont be added.
-        cy.get('.umb-node-preview__description').should('be.visible');
+        cy.get('.umb-node-preview__name').should('be.visible');
         //Save and publish
         cy.umbracoButtonByLabelKey('buttons_saveAndPublish').click();
-        
         cy.umbracoSuccessNotification().should('be.visible');
         //Waiting to ensure we have saved properly before leaving
-        cy.umbracoButtonByLabelKey('buttons_saveAndPublish').contains('Save and publish')
-        cy.umbracoButtonByLabelKey('buttons_saveAndPublish').should('be.visible');
         cy.reload();
         //Assert
         cy.get('.umb-notifications__notifications > .alert-error').should('not.exist');
         //Editing template with some content
-        cy.editTemplate(urlPickerDocTypeName, '@inherits Umbraco.Web.Mvc.UmbracoViewPage<ContentModels.UrlPickerTest>' +
-        '\n@using ContentModels = Umbraco.Web.PublishedModels;' +
-        '\n@{' +
-        '\n    Layout = null;' +
-        '\n}' +
-        '\n@foreach(var link in @Model.Picker)' +
-        '\n{' +
-        '\n    <a href="@link.Url">@link.Name</a>' +
-        '\n}');
         
         //Testing if the edits match the expected results
         const expected = '<a href="/">UrlPickerContent</a>';
