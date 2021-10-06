30e38229c0caf82d77cf3de9cc53527d550636bb
Nikolaj Geisle <niko737@edu.ucl.dk>
2021-10-06 13:03:01 +0200

    Exported method correctly

--- cypress/integration/HelpPanel/systemInformation.ts
+++ cypress/integration/HelpPanel/systemInformation.ts
@@ -21,24 +21,20 @@ context('System Information', () => {
         cy.contains('Current Culture').parent().should('contain', 'en-US');
         cy.contains('Current UI Culture').parent().should('contain', 'en-US');
     });
+    
+    it('Checks language displays correctly after switching', () => {
 
-    context('Language switching', () => {
-
-        it('Checks language displays correctly after switching', () => {
-
-            //Navigate to edit user and change language
-            cy.get('[data-element="global-user"]').click();
-            cy.get('[alias="editUser"]').click();
-            cy.get('[name="culture"]').select('string:da-DK', {timeout: 10000, force: true});
-            cy.umbracoButtonByLabelKey('buttons_save').click({force: true});
-            //Refresh site to display new language
-            cy.reload();
-            openSystemInformation();
-            //Assert
-            cy.contains('Current Culture').parent().should('contain', 'da-DK');
-            cy.contains('Current UI Culture').parent().should('contain', 'da-DK');
-            cy.get('.umb-button__content').last().click();
-            cy.reload();
-        });
+        //Navigate to edit user and change language
+        cy.get('[data-element="global-user"]').click();
+        cy.get('[alias="editUser"]').click();
+        cy.get('[name="culture"]').select('string:da-DK', {timeout: 10000, force: true});
+        cy.umbracoButtonByLabelKey('buttons_save').click({force: true});
+        //Refresh site to display new language
+        cy.reload();
+        openSystemInformation();
+        //Assert
+        cy.contains('Current Culture').parent().should('contain', 'da-DK');
+        cy.contains('Current UI Culture').parent().should('contain', 'da-DK');
+        cy.get('.umb-button__content').last().click();
     });
 });
