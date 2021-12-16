09c994eb9db81a7c07156edd097d939b1daf63c6
Mole <nikolajlauridsen@protonmail.ch>
2021-12-16 15:40:39 +0100

    Fix builder
    Assigned persmissions is not a list, it's an object

--- src/cms/builders/userGroups/userGroupBuilder.ts
+++ src/cms/builders/userGroups/userGroupBuilder.ts
@@ -1,18 +1,18 @@
 import faker from 'faker';
-import { AliasHelper } from 'src';
+import { AliasHelper } from '../../../helpers/aliasHelper';
 
-export class UserGroupBuilder{
-  alias : string;
-  assignedPermissions : string[];
-  defaultPermissions : string[];
-  icon : string;
-  id : number;
-  name : string;
-  parentId : number;
-  sections : string[];
-  startContentId : number;
-  startMediaId : number;
-  users : string[];
+export class UserGroupBuilder {
+  alias: string;
+  assignedPermissions: string[];
+  defaultPermissions: string[];
+  icon: string;
+  id: number;
+  name: string;
+  parentId: number;
+  sections: string[];
+  startContentId: number;
+  startMediaId: number;
+  users: string[];
   action: string;
 
   constructor() {
@@ -22,114 +22,113 @@ export class UserGroupBuilder{
     this.users = [];
   }
 
-  withAlias(alias){
+  withAlias(alias) {
     this.alias = alias;
     return this;
   }
 
-  withAssignedPermissions(assignedPermissions: string[]){
+  withAssignedPermissions(assignedPermissions: string[]) {
     this.assignedPermissions = assignedPermissions;
     return this;
   }
 
-  appendAssignedPermission(permission: string){
+  appendAssignedPermission(permission: string) {
     this.assignedPermissions.push(permission);
     return this;
   }
 
-  withDefaultPermissions(defaultPermissions: string[]){
+  withDefaultPermissions(defaultPermissions: string[]) {
     this.defaultPermissions = defaultPermissions;
     return this;
   }
 
-  appendDefaultPermission(defaultPermission: string){
+  appendDefaultPermission(defaultPermission: string) {
     this.defaultPermissions.push(defaultPermission);
     return this;
   }
 
-  withIcon(icon: string){
+  withIcon(icon: string) {
     this.icon = icon;
     return this;
   }
 
-  withId(id: number){
+  withId(id: number) {
     this.id = id;
     return this;
   }
 
-  withName(name: string){
+  withName(name: string) {
     this.name = name;
     return this;
   }
 
-  withParentId(parentId: number){
+  withParentId(parentId: number) {
     this.parentId = parentId;
     return this;
   }
 
-  withSections(sections: string[]){
+  withSections(sections: string[]) {
     this.sections = sections;
     return this;
   }
 
-  appendSection(section: string){
+  appendSection(section: string) {
     this.sections.push(section);
     return this;
   }
 
-  withStartContentId(startContentId: number){
+  withStartContentId(startContentId: number) {
     this.startContentId = startContentId;
     return this;
   }
 
-  withStartMediaId(startMediaId: number){
+  withStartMediaId(startMediaId: number) {
     this.startMediaId = startMediaId;
     return this;
   }
 
-  withUsers(users: string[]){
+  withUsers(users: string[]) {
     this.users = users;
     return this;
   }
 
-  appendUser(user: string){
+  appendUser(user: string) {
     this.users.push(user);
     return this;
   }
 
-  withAction(action: string){
+  withAction(action: string) {
     this.action = action;
     return this;
   }
 
-  withSave(){
-    this.action = "save";
+  withSave() {
+    this.action = 'save';
     return this;
   }
 
-  withSaveNew(){
-    this.action = "saveNew";
+  withSaveNew() {
+    this.action = 'saveNew';
     return this;
   }
 
-  build(){
+  build() {
     const name = this.name || faker.random.word;
     const alias = this.alias || AliasHelper.toSafeAlias(name);
 
     return {
-      action: this.action || "saveNew",
+      action: this.action || 'saveNew',
       alias: alias,
-      assignedPermissions: this.assignedPermissions || [],
+      assignedPermissions: {},
       defaultPermissions: this.defaultPermissions || [],
-      icon: this.icon || "icon-users",
+      icon: this.icon || 'icon-users',
       id: this.id || 0,
       name: name,
       parnetId: this.parentId || -1,
       sections: this.sections || [],
       startContentId: this.startContentId || null,
       startMediaId: this.startMediaId || null,
-      users: this.users || []
-    }
+      users: this.users || [],
+    };
   }
-
 }
