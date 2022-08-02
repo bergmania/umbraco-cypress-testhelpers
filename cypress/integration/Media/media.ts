350673e7d4d74017f16af0fac473e9c41acc8a13
Andreas Zerbst <andr317c@live.dk>
2022-08-02 10:14:03 +0200

    Added a command for EnsureMediaNameNotExists, also created a test for media

---
+++ cypress/integration/Media/media.ts
@@ -0,0 +1,32 @@
+﻿/// <reference types="Cypress" />	
+context('Media', () => {
+
+    beforeEach(() => {
+        cy.umbracoLogin(Cypress.env('username'), Cypress.env('password'));
+        cy.umbracoSection("media");
+    });
+
+   
+        it('Create folder', () => {
+         //const folderName =
+            
+            cy.get(".dropdown-toggle").contains("Create").click({force: true});
+            cy.get('[role="menuitem"]').contains("Folder").click({force: true});
+            cy.get('[data-element="editor-name-field"]').type('Media Folder');
+            cy.umbracoButtonByLabelKey("buttons_save").click();
+        });
+        
+
+    it('Create file inside of folder', () => {
+        //Wait to ensure the next button is clickable
+        cy.wait(1000)
+        cy.umbracoTreeItem('media', ['Media Folder']).click();
+        cy.get(".dropdown-toggle").contains("Create").click({force: true});
+        cy.get('[role="menuitem"]').contains("Folder").click({force: true});
+        cy.get('[data-element="editor-name-field"]').type('Folder in folder');
+        cy.umbracoButtonByLabelKey("buttons_save").click();
+        cy.umbracoEnsureMediaNameNotExists('Media Folder');
+       
+    });
+
+});
